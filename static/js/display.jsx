b36b04076a702ae5b9904fff8bc6e2e64ab143ca
Jeff Vincent <34778075+jeffreyvincent@users.noreply.github.com>
2018-11-06 20:25:41 -0800

    Create display.jsx

---
+++ static/js/display.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import uploadButton from "./uploadButton";
+
+export default class Display extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  callAPI() {
+    return console.log("method called");
+  }
+
+  render() {
+    return (
+      <div className="row height400 bg-blue rounded-corners blk-border">
+        <div>
+          <div className="row bg-lt-gry height300 twelve columns column rounded-corners dropshadow">
+            <p>This is the display frame</p>
+          </div>
+          <button className="button margin-30" onClick={this.callAPI}>
+            This is a display selector
+          </button>
+          <button className="button margin-30" onClick={this.callAPI}>
+            This is a display selector
+          </button>
+          <button className="button margin-30" onClick={this.callAPI}>
+            This is a display selector
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
