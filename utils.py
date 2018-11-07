e0f2ac6f163a517eb8cedf3bbd0dd66210abca12
Jeff Vincent <34778075+jeffreyvincent@users.noreply.github.com>
2018-11-06 20:08:18 -0800

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,6 @@
+""" This is where validation will go, and what ever else can go here. """
+def _is_valid_request(request):
+    if """TODO: figure out flow """:
+        return True
+    else:
+        return False
