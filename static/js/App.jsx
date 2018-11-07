0c2a74c6415f9d43f77822d62565e224e8cd69d8
Jeff Vincent <34778075+jeffreyvincent@users.noreply.github.com>
2018-11-06 20:22:44 -0800

    Fix

--- static/js/App.jsx
+++ static/js/App.jsx
@@ -1,7 +1,20 @@
-// App.jsx
 import React from "react";
+
+import Display from './display'
+import Controls from './controls'
+import uploadButton from './uploadButton'
+
 export default class App extends React.Component {
-  render () {
-    return <p> Hello React!</p>;
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <div className="container bg-lt-gry blk-border rounded-corners">
+        <Display />
+        <Controls />
+      </div>
+    );
   }
 }
