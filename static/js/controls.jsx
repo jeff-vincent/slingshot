85de1c86f1407ec0e588e8b1118ca810e8c869a9
Jeff Vincent <34778075+jeffreyvincent@users.noreply.github.com>
2018-11-12 13:09:55 -0800

    Fix

--- static/js/controls.jsx
+++ static/js/controls.jsx
@@ -1,17 +1,19 @@
-//import React from 'react';
-//
-//export default class Controls extends React.Component {
-//  constructor(props) {
-//    super(props);
-//  }
-//  
-//  render() {
-//    return (
-//      <div id="controls" className="row height400 bg-lt-gry rounded-corners blk-border">
-//        <div className="six columns rounded-corners ">
-//          <button className="button" onclick={this.openControls}> Open Control Panel </button>
-//      </div>
-//     </div>
-//    );
-//  }
-//}
+import React from 'react';
+
+class Controls extends React.Component {
+ constructor(props) {
+   super(props);
+ }
+ 
+ render() {
+   return (
+     <div id="controls" className="row height400 bg-lt-gry rounded-corners blk-border">
+       <div className="six columns rounded-corners ">
+         <button className="button" onclick={this.openControls}> Open Control Panel </button>
+     </div>
+    </div>
+   );
+ }
+}
+
+export default Controls
