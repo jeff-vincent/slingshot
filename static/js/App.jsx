f4f6dda487463e85aaebf8ea30aa0a1e7ebd048a
Jeff Vincent <34778075+jeffreyvincent@users.noreply.github.com>
2018-11-12 13:07:05 -0800

    Fix export

--- static/js/App.jsx
+++ static/js/App.jsx
@@ -1,17 +1,18 @@
-//import React from "react";
-//
-//import Display from './display'
-//import Controls from './controls'
-//
-//
-//export default class App extends React.Component {
-//  constructor(props) {
-//    super(props);
-//  }
-//  
-//  render() {
-//    return (
-//      <p> test </p>
-//    );
-//  }
-//}
+import React from "react";
+
+import Display from './display'
+import Controls from './controls'
+
+
+class App extends React.Component {
+ constructor(props) {
+   super(props);
+ }
+ 
+ render() {
+   return (
+     <p> test </p>
+   );
+ }
+}
+export default App
