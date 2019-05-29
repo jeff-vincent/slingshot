0893ec96f69c530ff976807e9df94bdc875f491f
Jeff Vincent <jeff.d.vincent@gmail.com>
2019-05-29 13:21:49 -0700

    Fix import

--- static/js/components/App.jsx
+++ static/js/components/App.jsx
@@ -1,18 +1,16 @@
 import React, {Component} from 'react';
 
-const App = function() {
-  return (
-    <div>
-      <h2>Header</h2>
-      <ul>
-        <li>Nav</li>
-        <li>Nav</li>
-        <li>Nav</li>
-      </ul>
-      <p>Body</p>
-      <h3>Footer</h3>
-    </div>
-  );
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div >
+        <p>Test</p>
+    	</div>)
+  }
+}
 
 export default App;
